b3ae6963634608c8bc7047d4ad2f04ecd5fa6b38
Dmitriy Dergachev <dergachev@vteleport.ru>
2017-03-14 18:20:17 +0400

    Переделки связанные с созданием новго проекта

---
+++ Components/Pages/Create/Settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author              Dmitriy Dergachev (ArtMares)
+ * @date                14.03.2017
+ * @copyright           artmares@influ.su
+ */
+namespace Components\Pages\Create;
+class Settings {
+
+}
